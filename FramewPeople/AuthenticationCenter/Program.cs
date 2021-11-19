6bd5a585775aa865e43da123fe45fe695b21c883
嘉毅 水 <2379646255@qq.com>
2021-11-19 16:01:21 +0800

    代码提交

--- FramewPeople/AuthenticationCenter/Program.cs
+++ FramewPeople/AuthenticationCenter/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace AuthenticationCenter
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
